38ffaceabaccb0751e6d032fc054159d94abd8fe
Diego Vaca <diegorve@yahoo.com>
2021-03-24 11:28:25 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+// querySelector() selecciona un elemento por clase
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show') // Añade o quita la clase .show
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.news-cards', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
